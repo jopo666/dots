09d8348558a51f06765e2a1ccad91125d266e244
joona <joona.pohjonen@aiforia.com>
2023-12-22 17:00:56 +0200

    remove i3

--- dotfiles/i3/lockscreen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-resolution=$(xrandr | awk '{print $8$9$10}' | head -n 1 | head -c -2)
-convert -resize $resolution $HOME/.config/i3/lockscreen.png /tmp/lockscreen.png
-i3lock -i /tmp/lockscreen.png -e -u
-sleep 600; pgrep i3lock && xset dpms force off
